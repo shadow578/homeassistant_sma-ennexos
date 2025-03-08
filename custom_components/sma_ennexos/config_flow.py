068876786d9853584345d65df16558b090b1a182
shadow578 <52449218+shadow578@users.noreply.github.com>
2025-03-08 09:40:09 +0000

    remove unused __fetch_available_channels from options flow

--- custom_components/sma_ennexos/config_flow.py
+++ custom_components/sma_ennexos/config_flow.py
@@ -6,7 +6,6 @@ from typing import Any
 from urllib.parse import urlparse
 
 import voluptuous as vol
-from attr import dataclass
 from homeassistant.config_entries import (
     ConfigEntry,
     ConfigFlow,
@@ -199,15 +198,6 @@ class SMAConfigFlow(ConfigFlow, domain=DOMAIN):
         return parsed_url.netloc
 
 
-@dataclass
-class OptionsFlowAvailableChannels:
-    """Available channels for selection."""
-
-    component_name: str | None
-    component_id: str
-    channel_id: str
-
-
 class SMAOptionsFlow(OptionsFlow):
     """options flow for SMA."""
 
@@ -276,60 +266,3 @@ class SMAOptionsFlow(OptionsFlow):
                 }
             ),
         )
-
-    async def __fetch_available_channels(
-        self,
-    ) -> list[OptionsFlowAvailableChannels]:
-        """Get a list of all available channels and their ids."""
-        host = self.config_entry.data[CONF_HOST]
-        LOGGER.debug("attempting to fetch available channels for host=%s", host)
-        sma = SMAApiClient(
-            host=host,
-            username=self.config_entry.data[CONF_USERNAME],
-            password=self.config_entry.data[CONF_PASSWORD],
-            session=async_create_clientsession(
-                hass=self.hass,
-                verify_ssl=self.config_entry.data[CONF_VERIFY_SSL],
-            ),
-            use_ssl=self.config_entry.data[CONF_USE_SSL],
-            request_timeout=self.config_entry.options.get(
-                OPT_REQUEST_TIMEOUT, DEFAULT_REQUEST_TIMEOUT
-            ),
-            request_retries=self.config_entry.options.get(
-                OPT_REQUEST_RETIRES, DEFAULT_REQUEST_RETIRES
-            ),
-            logger=LOGGER.getChild("options_sma_api"),
-        )
-
-        await sma.login()
-
-        # get components
-        all_components = await sma.get_all_components()
-
-        # fetch live data for all components
-        all_live_data = await sma.get_all_live_measurements(
-            component_ids=[component.component_id for component in all_components]
-        )
-        await sma.logout()
-
-        # return a dict for each live measurement
-        LOGGER.debug("found %s available channels before filtering", len(all_live_data))
-        result = [
-            OptionsFlowAvailableChannels(
-                component_name=next(
-                    (
-                        component.name
-                        for component in all_components
-                        if component.component_id == ld.component_id
-                    ),
-                    None,
-                ),
-                component_id=ld.component_id,
-                channel_id=ld.channel_id,
-            )
-            for ld in all_live_data
-            # ignore all entries that have no value in the latest measurement
-            if ld.latest_value.value is not None
-        ]
-        LOGGER.debug("found %s available channels after filtering", len(result))
-        return result
